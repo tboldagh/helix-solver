ac3f081f17c99c9fadf310d1b805fb26e28619cd
Adrian Wysocki <adrian.wyso.97@gmail.com>
2021-05-17 09:29:21 -0700

    changed acc size for testing

--- include/HelixSolver/Constants.h
+++ include/HelixSolver/Constants.h
@@ -1,8 +1,8 @@
 #pragma once
 
 static constexpr uint32_t MAX_STUB_NUM = 10000;
-static constexpr uint32_t ACC_WIDTH = 1000;
-static constexpr uint32_t ACC_HEIGHT = 3000;
+static constexpr uint32_t ACC_WIDTH = 10;
+static constexpr uint32_t ACC_HEIGHT = 30;
 static constexpr uint32_t ACC_SIZE = ACC_WIDTH * ACC_HEIGHT;
 
 static constexpr float Q_OVER_P_BEGIN  = -0.2857142857142857;
@@ -11,3 +11,4 @@ static constexpr float PHI_BEGIN = -3.641592653589793;
 static constexpr float PHI_END = 3.641592653589793;
 
 static constexpr float B = 2.0;
+
