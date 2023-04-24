f21c080e70d4dcb0132f09f6b13457a64e6f6581
Stefan Horodenski <shorodenski@student.agh.edu.pl>
2023-04-24 18:28:41 +0200

    Branch updates

---
+++ include/HelixSolver/Stub.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace HelixSolver
+{
+    struct Stub
+    {
+        const float x;
+        const float y;
+        const float z;
+        const uint8_t layer;
+    };
+
+} // HelixSolver
