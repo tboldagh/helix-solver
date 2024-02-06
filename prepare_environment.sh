3578b4e20d3083f97813987c79624d21219da2d6
Piotr Libucha <piotr.libucha.off@gmail.com>
2024-02-06 23:42:47 +0100

    Simplify environment, preparation and building
    Move codeplay to 3pp directory
    Add directories lib and repo in /helix in docker image
    Move some stages of environment preparation form Dockerfile to prepare_environment.sh
    Remove sourcing from build.sh
    Replace source_setvars.sh with prepare_environment.sh
    Update README

---
+++ prepare_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ../lib lib
+
+echo y | sh 3pp/codeplay/oneapi-for-nvidia-gpus-2023.2.1-cuda-12.0-linux.sh
+
+source /opt/intel/oneapi/setvars.sh --include-intel-llvm --force
+
+source lib/root/bin/thisroot.sh
